c475cf3ebf0a7930b69d3ecc54dff7d742c06167
Sepand Ansari <sepand@worksmartlabs.com>
2014-06-15 19:10:56 -0400

    mockup-ish implementation for msg page

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+var HappyHead = (function(document, $) {
+
+	var _hh = {};
+
+	_hh.init = function() {
+		console.log('init');
+		console.log($('.inbox'));
+
+	},
+
+	_hh.initMsgPage = function() {
+		console.log('msg init');
+		progressJs(".progress-bar").start().autoIncrease(5, 100).onbeforeend(function() {
+			console.log('progress end');
+		});
+
+		// callback not working settimout replacement
+		setTimeout(function() {
+			console.log('show button');
+
+		}, 2000);
+
+		//progressJs.onbeforeend(function() {
+		//	console.log('progress end');
+		//})
+		
+	}
+
+
+	return _hh;
+
+
+})(document, Zepto);
+
+
+//HappyHead.init();
