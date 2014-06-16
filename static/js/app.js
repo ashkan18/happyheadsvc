62a09b6c2ef955cbeebc763f2f74551757bcbe2c
Sepand Ansari <sepand@worksmartlabs.com>
2014-06-16 01:21:08 -0400

    progression

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,12 @@
-var HappyHead = (function(document, $) {
+var HappyHead = (function(document, window, $) {
 
 	var _hh = {};
 
 	_hh.init = function() {
 		console.log('init');
 		console.log($('.inbox'));
+
+		//window.addEventListener('push', function(e) { _pushCallback(e); });
 
 	},
 
@@ -13,32 +15,58 @@ var HappyHead = (function(document, $) {
 			document.location = 'inbox.html'
 
 		}, 2000);
+
+		console.log('add push');
+		console.log(window);
+
 	},
 
 
 	_hh.initMsgPage = function() {
 		console.log('msg init');
-		progressJs(".progress-bar").start().autoIncrease(5, 100).onbeforeend(function() {
+		progressJs(".progress-bar").start().autoIncrease(2, 100).onbeforeend(function() {
 			console.log('progress end');
 		});
 
 		// callback not working settimout replacement
 		setTimeout(function() {
 			console.log('show button');
+			$(".msg-cell").css('height','0px');
+			$(".msg-cell").css('padding','0px');
+			$(".msg-cell").css('opacity','0');
+			$(".sending").css('height','50px');
+			$(".sending").css('opacity','1');
+			$('.message .camera').css('opacity','1');
+			progressJs(".progress-bar").start().set(1).autoIncrease(2, 100).onbeforeend(function() {
+				console.log('progress end');
+			});
 
-		}, 2000);
+		}, 5000);
+
 
 		//progressJs.onbeforeend(function() {
 		//	console.log('progress end');
 		//})
 		
 	}
+/*
+	_hh.pushCallback = function(page, e) {
+		console.log('push callback');
 
+		console.log(e);
+		if(page=='inbox') {
+			setTimeout(function() {
+				_hh.initMsgPage();
+
+			}, 2000);
+		}
+	}
+*/
 
 	return _hh;
 
 
-})(document, Zepto);
+})(document, window, Zepto);
 
 
 //HappyHead.init();
